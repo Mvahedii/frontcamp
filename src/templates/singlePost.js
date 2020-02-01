26ecd6dd350f4df7f2a468d0e6d0d7024134cb90
Mvahedii <vahedi.ce@gmail.com>
2020-02-01 21:56:30 +0330

    styling single-post

--- src/templates/singlePost.js
+++ src/templates/singlePost.js
@@ -1,17 +1,45 @@
 import React from 'react'
 
+import { graphql } from 'gatsby'
+
 import Navbar from '../component/navbar/navbar'
 
-const SinglePost = () => {
+import './singlePost.css'
+
+const SinglePost = (props) => {
+
+    const data = props.data.markdownRemark.frontmatter
 
     return (
         <React.Fragment>
             <Navbar />
-            <h4>Hiiiii</h4>
+            <div className='article'>
+                <div className='article-image'>
+                    <img src={data.image} alt={data.title} />
+                </div>
+                <div className='article-title'>
+                    <h3>{data.title}</h3>
+                </div>
+                <div className='article-intro'>
+                    <p>{data.intro}</p>
+                </div>
+            </div>
         </React.Fragment>
 
     )
 
 }
+
+export const query = graphql`
+    query ($slug: String!) {
+        markdownRemark(fields: {slug: {eq: $slug}}) {
+            frontmatter {
+                title
+                intro
+                image
+    }
+  }
+}
+`
 
 export default SinglePost;
